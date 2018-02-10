f4aa9afdf0854c2d9842bf832a977e0cb353590f
Joe Li <joe@joe-mac.local>
2018-02-10 17:16:17 +0800

    add web to allow to upload and compress images

---
+++ image-utils-web/ecosystem.config.js
@@ -0,0 +1,47 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : 'image-utils-web',
+      script    : 'server.js',
+      // env: {  If we don't comment here, it will override below deploy config
+      //   PORT: 3011
+      // },
+      env_production : {
+        NODE_ENV: 'production'
+      }
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : 'joe',
+      host : '182.254.242.31',
+      ref  : 'origin/master',
+      repo : 'https://github.com/choelea/image-utils-web.git',
+      path : '/home/joe/nodejsapp/images-utils-web',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    },
+    dev : {
+      user : 'osboxes',
+      host : '192.168.1.186',
+      ref  : 'origin/master',
+      repo : 'https://github.com/choelea/image-utils-web.git',
+      path : '/home/osboxes/temp',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
+      env  : {
+        NODE_ENV: 'dev',
+		PORT: 3012
+      }
+    }
+  }
+};
---
+++ image-utils-web/routes/images.js
@@ -0,0 +1,73 @@
+let router = require('express').Router();
+var multer = require('multer');
+const uuidv1 = require('uuid/v1');
+const fs = require('fs');
+const im = require('imagemagick');
+var zipFolder = require('zip-folder');
+var upload = multer({ dest: 'uploads/tmp', limits: { fields: 10, fileSize: '20MB', files: 20 } })
+
+router.get('/', (req, res) => {
+    res.render('images/uploader', {
+        title: 'Compress Images',
+        myCSSs: ['images.css'],
+        myJSs: ['images-uploader.js']
+    });
+});
+
+router.get('/zip/:uuid', (req, res) => {
+    var images = [];
+    const uuid = req.params.uuid;
+    const dir = 'uploads/' + uuid;
+    var returnJson = {};
+    zipFolder(dir, 'uploads/' + uuid + ".zip", function (err) {
+        if (err) {
+            throw err
+        }
+    });
+    res.json({ 'success': true, 'url': '/uploads/' + uuid + ".zip" });
+});
+
+router.get('/:uuid/:count', (req, res) => {
+    const count = req.params.count;
+    const uuid = req.params.uuid;
+    var images = [];
+    const dir = 'uploads/' + uuid;
+    fs.readdir(dir, (err, files) => {
+        if (err) {
+            console.log(err);
+            throw error;
+        }
+        files.forEach(file => {
+            images.push('/uploads/' + uuid + '/' + file);
+        });
+    });
+    res.render('images/compressed-result', {
+        title: 'Compressed Images',
+        uuid: uuid,
+        images: images
+    });
+});
+
+router.get('/words', async (req, res) => {
+    const { userId } = req.query;
+    const wordsSnapshot = await db.ref(`words/${userId}`).once('value');
+    res.send(wordsSnapshot.val())
+});
+
+/** upload images: 1, upload to uploads/tmp */
+router.post('/upload', upload.array('images'), (req, res, next) => {
+    let uuid = uuidv1();
+    let targetFolder = 'uploads/' + uuid;
+    fs.mkdir(targetFolder, (error) => { if (error) throw error });
+    for (let i = 0; i < req.files.length; i++) {
+        const file = req.files[i]
+        const targetPath = targetFolder + "/" + file.originalname;
+        // fs.rename(file.path, targetPath, (error) => { if (error) throw error });
+
+        im.convert([file.path, '-quality', 80, targetPath], function (err, stdout) {
+            if (err) throw err;
+        })
+    }
+    res.json({ uuid, count: req.files.length });
+})
+module.exports = router;
